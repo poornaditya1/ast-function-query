f008fb345e066a4b5f16b17d0585615a5e4c0225
Poornaditya Mishra <42487913+poornaditya1@users.noreply.github.com>
2023-11-11 18:45:03 +0530

    Upload source code

---
+++ func_query.py
@@ -0,0 +1,18 @@
+import ast
+import sys
+
+def traverse_and_print_functions(node, text, indent=0):
+    if isinstance(node, ast.FunctionDef):
+        # Print function name and parameters
+        if node.name == text:
+            for statement in node.body:
+                print(ast.unparse(node).replace('\n', f'\n{" " * indent}'))
+
+    for child_node in ast.iter_child_nodes(node):
+        traverse_and_print_functions(child_node, text, indent=indent + 4)
+
+if __name__ == "__main__":
+    file = sys.argv[1]
+    func = sys.argv[2]
+
+    traverse_and_print_functions(file, func)
